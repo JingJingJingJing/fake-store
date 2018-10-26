83bbfeac7a6d85e0867d39852cd99383611cf1d3
Gian Zheng <zhengxp3@lenovo.com>
2018-10-26 11:08:23 +0800

    basiclly implement expand animation effect

--- js/today.js
+++ js/today.js
@@ -1,23 +1,63 @@
 "use strict";
-let articles = document.querySelectorAll('.app-card');
-for (let article of articles) {
-    article.addEventListener('click', fadingIn);
+let cards = document.querySelectorAll('.app-card');
+for (let card of cards) {
+    card.addEventListener('click', fadingIn);
 }
 function fadingIn(event) {
-    let article = event.target;
-    let rect = article.getBoundingClientRect();
-    let recommend = document.querySelector('.rc');
+    let card = event.target;
+    let rc = document.querySelector('.rc');
     let rcPircture = document.querySelector('.rc-picture');
     let rcContent = document.querySelector('.rc-content');
-    if (recommend && rcPircture && rcContent && article) {
-        recommend.setAttribute('style', 'display: block; top: ' + rect.top + 'px');
-        rcContent.innerHTML = "Here is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some content";
-        // disable scroll
-        let body = document.querySelector('body');
-        if (body)
-            body.style.overflow = 'hidden';
+    if (card && rc && rcPircture && rcContent) {
+        getRcArticle(rcContent);
+        expand(rc, card);
     }
     else {
         alert('The content you choosed is not exist anymore.');
     }
+}
+function expand(rc, card) {
+    let rect = card.getBoundingClientRect();
+    rc.setAttribute('style', 'display: block; top: ' + rect.top + 'px');
+    let scrollBack = rect.top > 0 ? -rect.top / 10 : 0;
+    rc.animate([
+        {
+            overflow: 'hidden',
+            width: '343.3px',
+            height: '400px',
+            borderRadius: '20px',
+            top: rect.top + 'px',
+            margin: '0 1rem 0',
+            easing: 'ease-out'
+        },
+        {
+            overflow: 'hidden',
+            width: '100%',
+            height: '100%',
+            borderRadius: '10px',
+            top: scrollBack + 'px',
+            margin: '0',
+            easing: 'ease-out'
+        },
+        {
+            overflow: 'auto',
+            width: '100%',
+            height: '100%',
+            borderRadius: '0px',
+            top: '0px',
+            margin: '0',
+            easing: 'ease-out'
+        }
+    ], {
+        duration: 500,
+        iterations: 1,
+        fill: "forwards"
+    });
+    // disable scroll
+    document.body.style.overflow = 'hidden';
+}
+function getRcArticle(rcContent) {
+    // TODO get article html from server
+    rcContent.innerHTML = "Here is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some content";
+    return true;
 }
--- src/today.ts
+++ src/today.ts
@@ -1,21 +1,64 @@
-let articles: NodeListOf<Element> = document.querySelectorAll('.app-card');
-for (let article of articles) {
-    article.addEventListener('click', fadingIn);
+let cards: NodeListOf<Element> = document.querySelectorAll('.app-card');
+for (let card of cards) {
+    card.addEventListener('click', fadingIn);
 }
+
 function fadingIn(event: Event) {
-    let article: EventTarget | null = event.target;
-    let rect: ClientRect = (<Element>article).getBoundingClientRect();
-    let recommend: HTMLElement | null = document.querySelector('.rc');
+    let card: EventTarget | null = event.target;
+    let rc: HTMLElement | null = document.querySelector('.rc');
     let rcPircture: HTMLElement | null = document.querySelector('.rc-picture');
     let rcContent: HTMLElement | null = document.querySelector('.rc-content');
-    if (recommend && rcPircture && rcContent && article) {
-        recommend.setAttribute('style', 'display: block; top: ' + rect.top + 'px');
-        rcContent.innerHTML = "Here is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some content";
-        // disable scroll
-        let body: HTMLElement | null = document.querySelector('body');
-        if (body)
-            body.style.overflow = 'hidden';
+
+    if (card && rc && rcPircture && rcContent) {
+        getRcArticle(rcContent);
+        expand(rc, <HTMLElement>card);
     } else {
         alert('The content you choosed is not exist anymore.');
     }
+}
+
+function expand(rc: HTMLElement, card: HTMLElement) {
+    let rect: ClientRect = card.getBoundingClientRect();
+    rc.setAttribute('style', 'display: block; top: ' + rect.top + 'px');
+    let scrollBack: number = rect.top > 0 ? -rect.top/10 : 0;
+        rc.animate([
+            {
+                overflow: 'hidden',
+                width: '343.3px',
+                height: '400px',
+                borderRadius: '20px',
+                top: rect.top + 'px',
+                margin: '0 1rem 0',
+                easing: 'ease-out'
+            } as Keyframe,
+            {
+                overflow: 'hidden',
+                width: '100%',
+                height: '100%',
+                borderRadius: '10px',
+                top: scrollBack + 'px',
+                margin: '0',
+                easing: 'ease-out'
+            } as Keyframe,
+            {
+                overflow: 'auto',
+                width: '100%',
+                height: '100%',
+                borderRadius: '0px',
+                top: '0px',
+                margin: '0',
+                easing: 'ease-out'
+            } as Keyframe
+        ], {
+                duration: 500,
+                iterations: 1,
+                fill: "forwards"
+            });
+    // disable scroll
+    document.body.style.overflow = 'hidden';
+}
+function getRcArticle(rcContent: HTMLElement): boolean{
+    // TODO get article html from server
+    rcContent.innerHTML = "Here is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some contentHere is just some content";
+    return true;
 }
