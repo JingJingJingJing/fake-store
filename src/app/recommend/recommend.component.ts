93e293aa82c8ec6c4922bce55d62d5ab2d92f5ca
Jing <summerrymaker@gmail.com>
2018-11-08 23:28:18 +0800

    roughly complete return animation

--- src/app/recommend/recommend.component.ts
+++ src/app/recommend/recommend.component.ts
@@ -45,31 +45,83 @@ export class RecommendComponent implements OnInit {
 
   initialDrag() {
     const rc: HTMLElement = document.querySelector('.rc');
-    rc.addEventListener('touchstart', event => {
-      const startPointer = event.touches[0];
-      rc.ontouchmove = function (e) {
-        const move: Touch = e.touches[0];
-        scale(startPointer.clientX, startPointer.clientY, move.clientX, move.clientY);
-      };
-    });
-    rc.addEventListener('touchend', event => {
-      rc.ontouchmove = null;
-      scale(0, 0, 0, 0);
-    });
+    initialDrag(rc);
   }
+}
 
+function initialDrag(rc: HTMLElement) {
+  rc.addEventListener('touchstart', event => {
+    const startPointer = event.touches[0];
+    rc.ontouchmove = function (e) {
+      const move: Touch = e.touches[0];
+      scale(startPointer.clientX, startPointer.clientY, move.clientX, move.clientY);
+    };
+  });
+  rc.addEventListener('touchend', resume);
+}
+
+function resume(event: Event) {
+  const rc = <HTMLElement>event.currentTarget;
+  rc.ontouchmove = null;
+  scale(0, 0, 0, 0);
 }
 
 function scale(startX: number, startY: number, x: number, y: number) {
   const rc: HTMLElement = document.querySelector('.rc');
-  if (rc.scrollTop === 0 && y >= startY) {
-    const scaleRate: string = 'scale(' + (1 - (y - startY) / rc.offsetHeight) + ')';
-    rc.animate({
-      transform: [scaleRate, scaleRate]
-    }, {
-        duration: 100,
-        iterations: 1,
-        fill: 'forwards'
+  const pullDown: Boolean = rc.scrollTop === 0 && y >= startY;
+  const swipeRight: Boolean = startX < 20;
+  if (!pullDown && !swipeRight)
+    return;
+  else {
+    const scaleRate: number = pullDown ? 1 - (y - startY) / rc.offsetHeight : 1 - (x - startX) / rc.offsetHeight;
+    if (scaleRate >= 0.9) {
+      rc.animate({
+        transform: [`scale(${scaleRate})`, `scale(${scaleRate})`]
+      }, {
+          iterations: 1,
+          fill: 'forwards'
+        }).play();
+    } else {
+      rc.animate({
+        transform: [`scale(0.9)`, `scale(0.9)`]
+      }, {
+          iterations: 1,
+          fill: 'forwards'
       }).play();
+      rc.ontouchmove = null;
+      rc.removeEventListener('touchend', resume);
+      rc.scrollTo({ top: 0, behavior: 'smooth' });
+      document.body.setAttribute('style', '');
+      const cardTop: number = parseFloat(rc.style.top.replace('px', ''));
+      console.log(cardTop * scaleRate);
+      rc.animate([
+        {
+          width: '100%',
+          top: 0,
+          height: '400px',
+          borderRadius: '10px'
+        } as AnimationKeyFrame,
+        {
+          width: '380px',
+          top: cardTop * 0.9 + 'px',
+          height: '440px',
+          borderRadius: '20px'
+        } as AnimationKeyFrame
+      ], {
+          duration: 300,
+          iterations: 1,
+          fill: 'forwards'
+      });
+      setTimeout(() => {
+        rc.animate({
+          transform: [`scale(0.9)`, `scale(1)`]
+        }, {
+            iterations: 1,
+            fill: 'forwards'
+        }).play();
+        initialDrag(rc);
+        rc.setAttribute('style', '');
+      }, 500);
+    }
   }
 }
